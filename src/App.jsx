f8f69b20bbf340f78f95e6350359d818b3fa847f
loki <lokeshyadav1098@gmail.com>
2025-02-18 15:25:12 +0530

    '

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ComingSoonPage from './comonents/ComingSoon'
 import './css/home.css'
-import './app.css'
+import './App.css'
 
 function App() {
 
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBHIz_EqJyM20dwWtJDwNMPHU9TPQEpCzg",
-  authDomain: "enipp-d0fd9.firebaseapp.com",
-  projectId: "enipp-d0fd9",
-  storageBucket: "enipp-d0fd9.firebasestorage.app",
-  messagingSenderId: "256362722040",
-  appId: "1:256362722040:web:f496c97b87c376e78e010d",
-  measurementId: "G-YF4B0WJLKB"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
