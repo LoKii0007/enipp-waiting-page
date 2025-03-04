221d1270b36c1ae4d6e4df3ff350cb4acfbffc14
loki <lokeshyadav1098@gmail.com>
2025-03-04 22:27:56 +0530

    '

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import ComingSoonPage from './comonents/ComingSoon'
 import './css/home.css'
 import './App.css'
+import { Toaster } from 'react-hot-toast'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<ComingSoonPage />} />
+        <Toaster position='top-right'/>
       </Routes>
     </Router>
   )
