3bd886f49d0155890beca943643d7dc0db9b3644
loki <lokeshyadav1098@gmail.com>
2025-03-05 10:43:47 +0530

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import ComingSoonPage from './comonents/ComingSoon'
-import './css/home.css'
-import './App.css'
-import { Toaster } from 'react-hot-toast'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ComingSoonPage from "./comonents/ComingSoon";
+import "./css/home.css";
+import "./App.css";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<ComingSoonPage />} />
-        <Toaster position='top-right'/>
       </Routes>
+
+      <Toaster position="top-right" />
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
